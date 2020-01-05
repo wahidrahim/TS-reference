9c41afbdc61bd286bbb976e9fdd37f30f9e1a793
Wahid Rahim <wahidrahim@gmail.com>
2020-01-05 03:50:30 -0500

    O(n) solution for same-squared

--- same-squared/index.ts
+++ same-squared/index.ts
@@ -8,24 +8,53 @@ import assert from 'assert'
  *******************************************************************************
  */
 
-const same = (arr1: number[], arr2: number[]) => {
+/**
+ * O(n)
+ */
+function same(arr1: number[], arr2: number[]) {
+  const arr1NumFrequency: { [key: string]: number } = {}
+  const arr2NumFrequency: { [key: string]: number } = {}
+
+  // O(n)
+  arr1.map(n => (arr1NumFrequency[n] = (arr1NumFrequency[n] || 0) + 1))
+  // O(n)
+  arr2.map(n => (arr2NumFrequency[n] = (arr2NumFrequency[n] || 0) + 1))
+
+  // O(n)
+  for (const key in arr1NumFrequency) {
+    const keySquared = Number(key) ** 2
+
+    if (!(keySquared in arr2NumFrequency)) return false
+    if (arr1NumFrequency[key] !== arr2NumFrequency[keySquared]) return false
+  }
+
+  return true
+}
+
+/**
+ * Naive solution
+ *
+ * O(n^2)
+ *
+function same(arr1: number[], arr2: number[]) {
   // Both arrays would have to be equal length
   if (arr1.length !== arr2.length) return false
 
   for (const n of arr1) {
     // Index of the squared number from arr1 in arr2
-    const nSquaredIndex = arr2.indexOf(n * n)
+    const nSquaredIndex = arr2.indexOf(n * n) // O(n)
 
-    if (nSquaredIndex >= 0) {
-      // If the squared number exists in arr2, remove it from arr2
-      arr2.splice(nSquaredIndex, 1)
-    }
+    if (nSquaredIndex === -1) return false
+
+    // If the squared number exists in arr2, remove it from arr2
+    arr2.splice(nSquaredIndex, 1) // O(n)
   }
 
-  // Arr2 would have to be empty by the end if it contained all the squared numbers from arr1
-  return arr2.length === 0
+  return true
 }
-
+*/
 assert(same([1, 2, 3], [4, 1, 9]) === true)
 assert(same([1, 2, 3], [1, 9]) === false)
 assert(same([1, 2, 1], [4, 4, 1]) === false)
+assert(same([3, 5, 2, 23, 21, 0, 32], [9, 25, 4, 529, 441, 0, 1024]) === true)
+assert(same([3, 5, 2, 23, 21, 0, 32], [9, 25, 4, 529, 441, 1, 1024]) === false)
