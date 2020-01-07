dae3fa02badd9b2c0b0c8cd0ba32617b49d104f0
Wahid Rahim <wahidrahim@gmail.com>
2020-01-06 22:52:47 -0500

    signly linked list

---
+++ data-structures/singly-linked-list/index.ts
@@ -0,0 +1,47 @@
+class ListNode<T> {
+  value: T
+  next: ListNode<T> | null
+
+  constructor(value: T) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  head: ListNode<any> | null
+  tail: ListNode<any> | null
+  length: number
+
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push<T>(value: T) {
+    const newNode = new ListNode(value)
+
+    if (this.tail) {
+      // Non-empty list
+      this.tail.next = newNode
+      this.tail = newNode
+    } else {
+      // Empty list
+      this.head = newNode
+      this.tail = this.head
+    }
+
+    this.length++
+
+    return this
+  }
+}
+
+const linkedList = new SinglyLinkedList()
+
+linkedList.push('hi')
+linkedList.push(1)
+linkedList.push('hellooo!')
+
+console.log(linkedList)
